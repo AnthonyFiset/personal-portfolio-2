50d9b91cd7769f7d5f5b97cfecaf1850fe8a395a
AnthonyFiset <tonyfise@my.yorku.ca>
2023-01-12 22:46:50 -0500

    Implemented navbar to main.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,17 +1,48 @@
+import { forwardRef } from 'react'
 import Logo from "./logo";
 import NextLink from "next/link";
 import { Container, Box, Link, Stack, Heading, Flex, Menu, MenuItem, MenuList, MenuButton, IconButton, useColorModeValue } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
-const LinkItem = ({ href, path, children }) => {
+const LinkItem = ({ href, path, target, children, ...props }) => {
     const active = path === href
     const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
     return (
-        <NextLink href={href}>
-            <Link p={2} bg={active ? 'glassTeal' : undefined} color={active ? '#202023' : inactiveColor} target={target} {...props}>
-                {children}
-            </Link>
-        </NextLink>
+      <Link
+        as={NextLink}
+        href={href}
+        scroll={false}
+        p={2}
+        bg={active ? 'grassTeal' : undefined}
+        color={active ? '#202023' : inactiveColor}
+        target={target}
+        {...props}
+      >
+        {children}
+      </Link>
     )
+  }
+  
+  const MenuLink = forwardRef((props, ref) => (
+    <Link ref={ref} as={NextLink} {...props} />
+  ))
+  
 
-}
+  const Navbar = props => {
+    const { path } = props
+  
+    return (
+      <Box
+        position="fixed"
+        as="nav"
+        w="100%"
+        bg={useColorModeValue('#ffffff40', '#20202380')}
+        css={{ backdropFilter: 'blur(10px)' }}
+        zIndex={2}
+        {...props}
+      >
+        </Box>
+    )
+    }
+
+    export default Navbar
